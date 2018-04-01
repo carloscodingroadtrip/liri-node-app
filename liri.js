bf55432b1961ad6e4376ca3542cde4267e8cb896
Carlos Molina <molina.carlos.dev@gmail.com>
2018-04-01 18:36:17 -0500

    Validation DONE

--- liri.js
+++ liri.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 
 var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var omdbApi = require('omdb-client');
+var fs = require("fs");
+var chalk = require("chalk");
+var date = require("date-and-time");
+
 
 // var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -8,3 +15,11 @@ var client = new Twitter(keys.twitter);
 var command = process.argv[2];
 var query   = process.argv[3];
 
+console.clear();
+//Validation for the command being entered by the user
+(command != undefined) ? command = command.toLowerCase() : console.log(chalk.bgYellow('ERROR, PLEASE KILL YOURSELF'));
+
+console.log(chalk.inverse('Ask LIRI any of the following: Tweets, Spotify, Movie, ToDo'));
+
+//Validate for
+(process.argv[4] != undefined) ? console.log(chalk.bgRed('ERROR, MULTI-WORDS HAVE TO BE SEPARATED BY DASH. ie: Home-alone')) : process.exit();
